ab0bdaf1073ebb81a3d4afdb0092efc4d45d3af0
Vladimir <stim_stim123321@mail.ru>
2024-09-05 11:00:50 +0500

    Second commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync')
-
+const rename = require('gulp-rename')
+const autoprefixer = require('gulp-autoprefixer')
+let cleanCss = require('gulp-clean-css')
 
 gulp.task('server', function() {
   browserSync.init({
@@ -15,6 +17,17 @@ gulp.task('server', function() {
 gulp.task('styles', function(){
   return gulp.src("src/sass/*.+(scss|sass)")
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({
+      prefix:"",
+      suffix:".min"
+    }))
+    .pipe(autoprefixer({
+      overrideBrowserslist: [
+          'last 2 version',
+          '> 1%'
+      ]
+  }))
+    .pipe(cleanCss({compatibility:"ie8"}))
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream())
 })
@@ -25,27 +38,3 @@ gulp.task('watch', function(){
 })
 
 gulp.task('default', gulp.parallel('watch','server',"styles"))
-
-// const paths = {
-//   styles: {
-//     src: 'src/sass/*.+(scss|sass)',
-//     dest: 'dist/css/'
-//   },
-//   scripts:{
-//     src: 'src/styles/**/*.js',
-//     dest: 'dist/js/'
-//   }
-// }
-
-// function clean(){
-//   return del(['dist'])
-// }
-
-// function styles() {
-//   return gulp.src(paths.styles.src)
-//   .pipe(sass().on('error', sass.logError))
-//   .pipe(gulp.dest(paths.styles.dest))
-// }
-
-// exports.clean = clean
-// exports.styles = styles
